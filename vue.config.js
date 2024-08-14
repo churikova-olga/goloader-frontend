1f00d84d76d65aeeb50326f596580ee0c6fdfdbb
churikova-olga <olga_churikova890@mail.ru>
2024-08-14 22:10:07 +0200

    update vue

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,13 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // outputDir: path.resolve(__dirname, '../backend/public'),
-  // devServer: {
-  //   proxy:{
-  //     '/api':{
-  //       target: 'http://localhost:3000'
-  //     }
-  //   }
-  // }
+  devServer: {
+    server: {
+      type: 'https', // Укажите, что сервер должен быть HTTPS
+      // options: { // Дополнительные параметры HTTPS
+      //   key: path.resolve(__dirname, 'your-key.pem'),
+      //   cert: path.resolve(__dirname, 'your-cert.pem')
+      // }
+    }
+  }
 })
